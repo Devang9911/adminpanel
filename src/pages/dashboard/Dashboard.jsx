4c491a1e5152ff73ec05fd86132a9cc1b2d8e499
Devang_Randeria <devangranderia9@gmail.com>
2026-04-13 11:17:02 +0530

    dashboard section design change

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -4,7 +4,6 @@ import {
   FolderIcon,
   UsersIcon,
 } from "@heroicons/react/24/solid";
-
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
@@ -30,36 +29,33 @@ function Dashboard() {
   }
 
   return (
-    <div className="w-full space-y-3">
+    <div className="w-full space-y-4">
       <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-4 gap-4">
         <StatsBox
           value={summary?.totalUsers || 0}
           title="Total Users"
           icon={UsersIcon}
-          color="bg-blue-500"
+          color="bg-indigo-500"
           onClick={() => navigate("/users")}
         />
-
         <StatsBox
           value={summary?.totalWorkspaces || 0}
           title="Workspaces"
           icon={FolderIcon}
-          color="bg-yellow-500"
+          color="bg-amber-500"
           onClick={() => navigate("/workspaces")}
         />
-
         <StatsBox
           value={summary?.activeSubscriptions || 0}
-          title="Active Subscriptions"
+          title="Active subscriptions"
           icon={CheckBadgeIcon}
-          color="bg-green-500"
+          color="bg-emerald-500"
         />
-
         <StatsBox
           value={`₹ ${summary?.totalRevenue || 0}`}
           title="Total Revenue"
           icon={CurrencyRupeeIcon}
-          color="bg-orange-500"
+          color="bg-pink-500"
         />
       </div>
     </div>
